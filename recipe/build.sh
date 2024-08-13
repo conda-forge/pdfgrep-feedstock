3ebe5dc4e27c4722289a358cefbc5c008565e48f
Uwe Korn <uwelk@xhochy.com>
2024-08-13 09:42:33 +0000

    Pin to older libgcrypt

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,6 @@
 
 set -exuo pipefail
 
-./configure --prefix=$PREFIX || (cat config.log; exit 1)
+./configure --prefix=$PREFIX --with-libgcrypt-prefix=$PREFIX || (cat config.log; exit 1)
 make -j$CPU_COUNT
 make install
